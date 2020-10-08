e38bd037d2ddc969579d00de8d5bb6d807d3de0a
zekinah <zjlecaros@gmail.com>
2020-10-09 07:52:23 +0800

    Released version 1.0.7

--- includes/class-zone-cookie.php
+++ includes/class-zone-cookie.php
@@ -74,7 +74,7 @@ class Zone_Cookie {
 		if ( defined( 'Zone_Cookie_VERSION' ) ) {
 			$this->version = Zone_Cookie_VERSION;
 		} else {
-			$this->version = '1.0.6';
+			$this->version = '1.0.7';
 		}
 		$this->plugin_name = 'zone-cookie';
 
